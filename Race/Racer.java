302cf7943a37720f18dece0930437f0ec9fc35bf
Evgeny865 <157610686+Evgeny865@users.noreply.github.com>
2024-01-26 22:42:02 +0200

    I think it is work 1

--- Race/Racer.java
+++ Race/Racer.java
@@ -8,14 +8,15 @@ public class Racer implements Runnable {
     private static final Object lock = new Object();
 
     public Racer(int speed, Track track) {
-            this.id = getNextId();
-            this.track = track;
-            if (!checkSpeed(speed)) {
-                throw new IllegalArgumentException("Speed must be between 1 and 10");
-            }
-            this.speed = speed;
+        this.id = getNextId();
+        this.track = track;
+        if (!checkSpeed(speed)) {
+            throw new IllegalArgumentException("Speed must be between 1 and 10");
+        }
+        this.speed = speed;
 
     }
+
     private static int getNextId() {
         synchronized (lock) {
             return globalId++;
@@ -26,37 +27,30 @@ public class Racer implements Runnable {
         return speed >= 1 && speed <= 10;
 
     }
-    private void runMeters(int meters){
-            System.out.println("Runner: " + id + " ran " + meters + " meters" + Thread.currentThread());
 
-
-    }
 
     public void go() throws InterruptedException {
 
         for (int i = 1; i <= 100; i++) {
-                runMeters(i);
-                    if (i == 100) {
-                        synchronized (Track.class) {
-                            Track.finishRacers++;
-                            System.out.println("Runner: " + id + " finished " + i + " meters " +
-                                    Track.finishRacers + getPlace(Track.finishRacers));
-                        }
-                    }
+            System.out.println("Runner: " + id + " ran " + i + " meters" );
+            synchronized (Track.class){
+            if (i == 100) {
+                    Track.finishRacers++;
+                    System.out.println("Runner: " + id + " finished " + i + " meters " +
+                            Track.finishRacers + getPlace(Track.finishRacers));
+                }
+            }
         }
     }
-   private String getPlace(int numberPlace) {
 
-        switch (numberPlace % 10) {
-            case 1:
-                return "st";
-            case 2:
-                return "nd";
-            case 3:
-                return "rd";
-            default:
-                return "th";
-        }
+    private String getPlace(int numberPlace) {
+
+        return switch (numberPlace %10) {
+            case 1 -> "st";
+            case 2 -> "nd";
+            case 3 -> "rd";
+            default -> "th";
+        };
     }
 
     @Override
